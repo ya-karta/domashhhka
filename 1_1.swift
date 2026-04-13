a35b0d3b7a8f895ca9a89751b91f966cbc833c85
ya-karta <anastasiya.solovaya@gmail.com>
2026-04-13 22:43:45 +0300

    Первый коммит

---
+++ 1_1.swift
@@ -0,0 +1,17 @@
+import Foundation
+var a:Double = 10
+var b:Double  = 4
+var c:Double  = 6
+var D:Double  = b*b-4a*c
+if D<0 {
+    print ("no resheniy")
+} else {
+    if D ==0 {
+        var x:Double = (-b/2*a)
+        print (x)
+    } else {
+        var x1:Double = (-b+sqrt(D))/2*a
+        var x2:Double = (-d-sqrt(D))/2*a
+        print (x1,x2)
+    }
+}
