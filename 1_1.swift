3d840a4388c878d36095b797a3bb62b94ac1be5e
ya-karta <anastasiya.solovaya@gmail.com>
2026-04-13 22:53:11 +0300

    Правки 1_1

--- 1_1.swift
+++ 1_1.swift
@@ -2,16 +2,16 @@ import Foundation
 var a:Double = 10
 var b:Double  = 4
 var c:Double  = 6
-var D:Double  = b*b-4a*c
+var D:Double  = b*b-4*a*c
 if D<0 {
     print ("no resheniy")
 } else {
-    if D ==0 {
+    if D==0 {
         var x:Double = (-b/2*a)
         print (x)
     } else {
         var x1:Double = (-b+sqrt(D))/2*a
-        var x2:Double = (-d-sqrt(D))/2*a
-        print (x1,x2)
+        var x2:Double = (-b-sqrt(D))/2*a
+        print (x1, x2)
     }
 }
